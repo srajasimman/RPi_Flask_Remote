82653358df556dc40d396ec1826381e8b5141f3e
Rajasimman S <srajasimman@gmail.com>
2018-03-09 15:14:21 +0530

    relay1 and relay2 added.

--- app.py
+++ app.py
@@ -9,81 +9,57 @@ app = Flask(__name__)
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 
-#define sensors GPIOs
-button = 20
-senPIR = 16
-
 #define actuators GPIOs
-ledRed = 13
-ledYlw = 19
-ledGrn = 26
+relay1 = 23
+relay2 = 24
 
 #initialize GPIO status variables
-buttonSts = 0
-senPIRSts = 0
-ledRedSts = 0
-ledYlwSts = 0
-ledGrnSts = 0
+relay1_Sts = 0
+relay2_Sts = 0
 
 # Define button and PIR sensor pins as an input
-GPIO.setup(button, GPIO.IN)   
+GPIO.setup(button, GPIO.IN)
 GPIO.setup(senPIR, GPIO.IN)
 
 # Define led pins as output
-GPIO.setup(ledRed, GPIO.OUT)   
-GPIO.setup(ledYlw, GPIO.OUT) 
-GPIO.setup(ledGrn, GPIO.OUT) 
+GPIO.setup(relay1, GPIO.OUT)
+GPIO.setup(relay2, GPIO.OUT)
 
-# turn leds OFF 
-GPIO.output(ledRed, GPIO.LOW)
-GPIO.output(ledYlw, GPIO.LOW)
-GPIO.output(ledGrn, GPIO.LOW)
-	
+# turn leds OFF
+GPIO.output(relay1, GPIO.LOW)
+GPIO.output(relay2, GPIO.LOW)
+
 @app.route("/")
 def index():
 	# Read GPIO Status
-	buttonSts = GPIO.input(button)
-	senPIRSts = GPIO.input(senPIR)
-	ledRedSts = GPIO.input(ledRed)
-	ledYlwSts = GPIO.input(ledYlw)
-	ledGrnSts = GPIO.input(ledGrn)
+	relay1_Sts = GPIO.input(relay1)
+	relay2_Sts = GPIO.input(relay2)
 
 	templateData = {
-      'button'  : buttonSts,
-      'senPIR'  : senPIRSts,
-      'ledRed'  : ledRedSts,
-      'ledYlw'  : ledYlwSts,
-      'ledGrn'  : ledGrnSts,
+      'relay1'  : relay1_Sts,
+      'relay2'  : relay2_Sts,
       }
 	return render_template('index.html', **templateData)
-	
+
 # The function below is executed when someone requests a URL with the actuator name and action in it:
 @app.route("/<deviceName>/<action>")
 def action(deviceName, action):
-	if deviceName == 'ledRed':
-		actuator = ledRed
-	if deviceName == 'ledYlw':
-		actuator = ledYlw
-	if deviceName == 'ledGrn':
-		actuator = ledGrn
-   
+	if deviceName == 'relay1':
+		actuator = relay1
+	if deviceName == 'relay2':
+		actuator = relay2
+
 	if action == "on":
 		GPIO.output(actuator, GPIO.HIGH)
 	if action == "off":
 		GPIO.output(actuator, GPIO.LOW)
-		     
-	buttonSts = GPIO.input(button)
-	senPIRSts = GPIO.input(senPIR)
-	ledRedSts = GPIO.input(ledRed)
-	ledYlwSts = GPIO.input(ledYlw)
-	ledGrnSts = GPIO.input(ledGrn)
-   
+
+	relay1_Sts = GPIO.input(relay1)
+	relay2_Sts = GPIO.input(relay2)
+
 	templateData = {
-	  'button'  : buttonSts,
-      'senPIR'  : senPIRSts,
-      'ledRed'  : ledRedSts,
-      'ledYlw'  : ledYlwSts,
-      'ledGrn'  : ledGrnSts,
+      'relay1'  : relay1_Sts,
+      'relay2'  : relay2_Sts,
 	}
 	return render_template('index.html', **templateData)
 
